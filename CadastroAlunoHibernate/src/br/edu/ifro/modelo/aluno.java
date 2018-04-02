e7fcc660c79a12a1bfbdbcaec57d5f9199fe5b8b
02977022201 <02977022201@JIPA-LABIN05-18.ifro.local>
2018-04-02 09:10:00 -0400

    Instalação do hibernate

---
+++ CadastroAlunoHibernate/src/br/edu/ifro/modelo/aluno.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author 02977022201
+ */
+@Entity
+public class aluno {
+    
+    
+    private Integer id,idade;
+    private String nome;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
